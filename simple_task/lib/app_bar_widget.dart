d9264a5594159c5cfb269962164b2f086f88f877
usmanarif534 <usmanarif534@gmail.com>
2022-02-19 16:11:41 +0500

    deletion of simple task

--- simple_task/lib/app_bar_widget.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-PreferredSizeWidget buildAppBar(String title) {
-  return AppBar(
-    centerTitle: true,
-    title: Text(title),
-    backgroundColor: Colors.deepOrange,
-  );
-}
