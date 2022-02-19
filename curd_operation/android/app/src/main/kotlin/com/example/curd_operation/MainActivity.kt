a57cfd15a577c6e5c0d4c538daa1c1d0b002c542
usmanarif534 <usmanarif534@gmail.com>
2022-02-19 16:14:23 +0500

    delition of 2 folders (curd operation & theme changer)

--- curd_operation/android/app/src/main/kotlin/com/example/curd_operation/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.curd_operation
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- theme_changer/android/app/src/main/kotlin/com/example/theme_changer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.theme_changer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
